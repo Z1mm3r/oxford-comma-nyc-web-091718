727a7917dbd7e06fda34180dfdb62775aaba5468
Casey Zimmer <czimmer@ramapo.edu>
2018-09-13 23:05:30 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -12,14 +12,14 @@ def oxford_comma(array)
       
     when array.size > 2 
       array.each_with_index |element,index|
-      {
+      
       
         if(index == output_string.size - 2)
           output_string += ", and " + element
         else
           output_string += ", " + element
         end
-      }
+      
       end
   
   end
