6a73e3df7449d379e1dc06359a2b6a1ab7257d75
Casey Zimmer <czimmer@ramapo.edu>
2018-09-13 23:22:33 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -13,7 +13,7 @@ def oxford_comma(array)
     when array.size > 2 
       array.each_with_index do |element,index|
       temp_hold = []
-      tempHold.push(array[index])
+      temp_Hold.push(array[index])
         if(index == 0)
           output_string += temp_hold.join("")
           
